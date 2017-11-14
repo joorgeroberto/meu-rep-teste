bbf7c6ab37b38d48c4bd3261d335f5ebfce8f0b1
Jorge Roberto <jorgercj@dcomp.ufs.br>
2017-11-14 20:50:25 +0000

    Meu primeiro commit

---
+++ src/main/java/br/ufs/dcomp/ProjetoTeste/App.java
@@ -0,0 +1,13 @@
+package br.ufs.dcomp.ProjetoTeste;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
